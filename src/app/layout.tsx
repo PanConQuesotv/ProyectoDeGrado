d43ec06532fa8af03a2827c4c4ff32f043e072a7
PanConQueso <sebastiangarzongomez12@gmail.com>
2025-11-25 09:57:27 -0500

    Initial commit: Next.js project setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,5 @@
-// app/layout.tsx
 import './global.css';
 import { ReactNode } from 'react';
-
-export const metadata = {
-  title: 'Simulador CNC',
-  description: 'Simulador de Fresadora CNC RA',
-};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
